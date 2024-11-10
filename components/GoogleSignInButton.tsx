575b2c39d8a9a26ade9618511f631b76e7db5614
Ching Jui Young <young.chingjui@gmail.com>
2024-11-10 10:43:12 +0800

    added auth

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,25 +1,26 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { signIn } from "next-auth/react";
+import { signIn } from "@/auth";
 
 function GoogleSignInButton() {
   return (
-    <Button
-      variant="outline"
-      className="w-full"
-      onClick={() => signIn("google")}
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
     >
-      <Image
-        src="/google-logo.svg"
-        width={20}
-        height={20}
-        alt="Google logo"
-        className="mr-2"
-      />
-      Sign in with Google
-    </Button>
+      <Button variant="outline" className="w-full" type="submit">
+        <Image
+          src="/google-logo.svg"
+          width={20}
+          height={20}
+          alt="Google logo"
+          className="mr-2"
+        />
+        Sign in with Google
+      </Button>
+    </form>
   );
 }
 
