074ef19286dbc3c871d0bb8ab1f9bbedfb7a10ed
Ching Jui Young <young.chingjui@gmail.com>
2024-11-10 23:00:06 +0800

    logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CalendarWrappedComponent } from "@/components/calendar-wrapped";
 import { auth } from "@/auth";
-import {redirect} from "next/navigation"
+import { redirect } from "next/navigation";
+import LogoutButton from "@/components/LogoutButton";
 
 export default async function Page() {
   const session = await auth();
@@ -9,5 +10,10 @@ export default async function Page() {
     redirect("/login");
   }
 
-  return <CalendarWrappedComponent />;
+  return (
+    <>
+      <CalendarWrappedComponent />
+      <LogoutButton />
+    </>
+  );
 }
---
+++ components/LogoutButton.tsx
@@ -0,0 +1,21 @@
+import { signOut } from "@/auth";
+import { Button } from "./ui/button";
+
+export default function LogoutButton() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <Button
+        variant="outline"
+        type="submit"
+        className="fixed bottom-4 right-4 px-4 py-2 rounded shadow"
+      >
+        Logout
+      </Button>
+    </form>
+  );
+}
