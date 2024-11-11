65b41677bdf00c2a797b66437776ee51b6eaa93b
Ching Jui Young <young.chingjui@gmail.com>
2024-11-11 10:28:31 +0800

    got events

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,31 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import LogoutButton from "@/components/LogoutButton";
 
+const getCalendarData = async (userId: string) => {
+  const session = await auth();
+  const calendarId = "primary";
+  const calendarData = await fetch(
+    `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+    {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    }
+  );
+  return calendarData.json();
+}
+
 export default async function Page() {
   const session = await auth();
 
   if (!session) {
     redirect("/login");
   }
+
+  // Here, we should be logged into Google. Let's get calendar data
+
+  const calendarData = await getCalendarData(session.user.id);
+  console.log(calendarData);
 
   return (
     <>
--- auth.ts
+++ auth.ts
@@ -5,7 +5,27 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    authorization: {
+      params: {
+        scope: "openid email profile https://www.googleapis.com/auth/calendar.events.readonly",
+        prompt: "consent",
+        access_type: "offline",
+      },
+    },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+  callbacks: {
+    async jwt({ token, user, account }) {
+
+      // Initial sign in
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {...session, accessToken: token.accessToken};
+    },
+  },
+});
